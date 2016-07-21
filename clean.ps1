8d766694c68b493ab5bc778e7362d7c8c62cad1a
Brady Gaster <bradyg@microsoft.com>
2016-07-21 13:57:05 -0700

    removed files

--- clean.ps1
+++ clean.ps1
@@ -1,11 +1,8 @@
 Remove-Item _site\* -Recurse
-Remove-Item .optemp\* -Recurse
-Remove-Item azure\* -Recurse
-Remove-Item log\* -Recurse
-Remove-Item obj\* -Recurse
-Remove-Item api\* -Exclude "index.md" -Recurse
 Remove-Item _site
+Remove-Item .optemp\* -Recurse
 Remove-Item .optemp
-Remove-Item azure
+Remove-Item log\* -Recurse
 Remove-Item log
+Remove-Item obj\* -Recurse
 Remove-Item obj
