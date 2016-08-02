7da7d29c02e088523e28a0821018b85ab19d0c95
Brady Gaster <bradyg@microsoft.com>
2016-08-01 23:35:18 -0700

    added basic .net console project

---
+++ src/SampleApp/SampleApp.ConsoleApp/Models.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp.ConsoleApp
+{
+    /// <summary>
+    /// Provides an ontological system for the application domain.
+    /// </summary>
+    public class AppModel
+    {
+        /// <summary>
+        /// A basic property just being used to show the basic functionality. 
+        /// </summary>
+        public string BasicProperty { get; set; }
+
+        /// <summary>
+        /// Does some unit of work.
+        /// </summary>
+        /// <param name="options">Options for this method call</param>
+        public void DoSomething(DoSomethingParameters options)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Parameters object used by the AppModel.DoSomething() event.
+    /// <seealso cref="AppModel.DoSomething"/>
+    /// </summary>
+    public class DoSomethingParameters
+    {
+        /// <summary>
+        /// The ID of this method request.
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
---
+++ src/SampleApp/SampleApp.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp.ConsoleApp
+{
+    /// <summary>
+    /// The application's main class.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Starting point for the app.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
