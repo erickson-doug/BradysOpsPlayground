cffaa77ae881bae9f7c9e831b9ffcf5d1049ef4b
Brady Gaster <bradyg@microsoft.com>
2016-08-08 15:32:21 -0700

    added new project

---
+++ src/RobotFactory/Services/IPersonRepository.cs
@@ -0,0 +1,16 @@
+﻿using RobotFactory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotFactory.Services
+{
+    public interface IPersonRepository
+    {
+        IEnumerable<Person> GetPeople();
+        bool CreatePerson(Person person);
+        bool DeletePerson(int personId);
+    }
+}
---
+++ src/RobotFactory/Services/MockPersonRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RobotFactory.Models;
+
+namespace RobotFactory.Services
+{
+    public class MockPersonRepository : IPersonRepository
+    {
+        public bool CreatePerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeletePerson(int personId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> GetPeople()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
