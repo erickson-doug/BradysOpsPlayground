a6e77c529aec62676b95bd2a53c959c6f97fc010
Brady Gaster <bradyg@microsoft.com>
2016-08-08 22:18:06 -0700

    manual merge

---
+++ clean.ps1
@@ -0,0 +1,10 @@
+#Remove-Item _themes\* -Recurse
+#Remove-Item _themes
+Remove-Item _site\* -Recurse
+Remove-Item _site
+Remove-Item .optemp\* -Recurse
+Remove-Item .optemp
+Remove-Item log\* -Recurse
+Remove-Item log
+Remove-Item obj\* -Recurse
+Remove-Item obj
