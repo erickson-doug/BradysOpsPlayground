1352ef99b12277bbed152228ccee95d69248f483
Brady Gaster <bradyg@microsoft.com>
2016-08-24 10:00:48 -0700

    incremental change

--- src/RobotFactory/Services/IRobotFactory.cs
+++ src/RobotFactory/Services/IRobotFactory.cs
@@ -18,15 +18,19 @@ namespace RobotFactory.Services
         /// <param name="robot">The <paramref name="robot"/> parameter is the robot you want to install.</param>
         /// <returns></returns>
         bool AttachRobot(Robot robot);
+
         bool DetachRobot(int robotId);
         bool RechargeRobot(int robotId);
         bool UseRobot(int robotId);
 
         /// <summary>
-        /// Method used to simply test out the <typeparamref name="T"/>.
+        /// Method used to simply test out the <typeparamref name="T"/>. 
         /// </summary>
         /// <typeparam name="T">The type to be handled.</typeparam>
         /// <param name="input">The instance to pass in.</param>
+        /// <remarks>
+        /// Represents the status of an asynchronous <languageKeyword>operation</languageKeyword> and provides support for cancellation.
+        /// </remarks>
         void SomeGenericMethod<T>(T input);
 
         event EventHandler<Robot> RobotUsed;
